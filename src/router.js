8ddf68f6c5adacd9216d2f84830ec45012e1503a
Mauricio Holanda <maumauholanda0@gmail.com>
2020-04-04 11:25:12 -0300

    Do Header Form

--- src/router.js
+++ src/router.js
@@ -4,7 +4,8 @@ import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import Logon from "./pages/Logon";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-import NewIncident from "./pages/NewIncident";
+// import NewIncident from "./pages/NewIncident";
+import Form from "./pages/Form/Checkout";
 import {isAutenticated} from "./services/auth";
 
 const PrivateRoute = ({component: Component, ...rest}) => (
@@ -26,7 +27,7 @@ const Routes = () => (
       <Route path="/" exact component={Logon} />
       <Route path="/register" component={Register} />
       <PrivateRoute path="/profile" component={Profile} />
-      <PrivateRoute path="/incidents/new" component={NewIncident} />
+      <PrivateRoute path="/Form/new" component={Form} />
     </Switch>
   </BrowserRouter>
 );
