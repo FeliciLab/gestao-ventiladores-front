b9264bded47491be9112c4c6fab5609a39cbcf16
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-06 18:01:12 -0300

    #1 Adicionado a lista baseados nas OS

--- src/pages/OsPrinter/OsPrinter.js
+++ src/pages/OsPrinter/OsPrinter.js
@@ -3,18 +3,18 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
 import React from 'react';
-// import {Redirect} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 
-const OsPrinter = () => {
+const OsPrinter = (props) => {
   const testeCsv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vReGuRPHIq68zmQ-9iGBLxVKFGoa0FcC8OU5FbCkhcUmdMFoRkbrMyAV2ZYsbcOiE87kTlK6RKpo4h2/pub?output=csv'
   const [csv, setName] = React.useState(testeCsv);
-  const [url, setUrl] = React.useState('/osprint?url=' + testeCsv);
+  const [url, setUrl] = React.useState('/oslist?url=' + testeCsv);
   const [toprint, setToprint] = React.useState(false);
 
   const handleChange = (event) => {
-    const url = changeToDownloadableCSV(event.target.value)
+    const url = changeToDownloadableCSV(event.target.value);
     setName(url);
-    setUrl(`/osprint?url=${url}`)
+    setUrl(url);
   };
 
   const changeToDownloadableCSV = (url) => {
@@ -31,7 +31,7 @@ const OsPrinter = () => {
   };
 
   if (toprint) {
-    window.open(url)
+    return <Redirect to={url}/>
   }
 
   return (
