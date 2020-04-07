16d4b59bafeb73e9f6fedacbcf4417f12886caa7
Mauricio Holanda <maumauholanda0@gmail.com>
2020-04-06 21:13:13 -0300

    Limpando o codigo e criando a tela de Recebimento

---
+++ src/pages/Form/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../_layout/Layout";
+import Formulario from "./Formulario";
+export default function () {
+  return (
+    <Layout>
+      <Formulario />
+    </Layout>
+  );
+}
--- src/router.js
+++ src/router.js
@@ -1,19 +1,17 @@
 import React from "react";
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 
-// import Logon from "./pages/Logon";
-import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import OsPrinter from "./pages/OsPrinter";
 import OsPrint from "./pages/OsPrint";
-// import NewIncident from "./pages/NewIncident";
-import Form from "./pages/Form/Checkout";
+
+import Form from "./pages/Form";
 import {isAutenticated} from "./services/auth";
 
 const PrivateRoute = ({component: Component, ...rest}) => (
   <Route
     {...rest}
-    render={props =>
+    render={(props) =>
       isAutenticated() ? (
         <Component {...props} />
       ) : (
@@ -26,13 +24,11 @@ const PrivateRoute = ({component: Component, ...rest}) => (
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      {/*<Route path="/" exact component={Logon} />*/}
       <Route path="/" exact component={OsPrinter} />
-      <Route path="/register" component={Register} />
       <Route path="/osprinter" component={OsPrinter} />
       <Route path="/osprint" component={OsPrint} />
       <PrivateRoute path="/profile" component={Profile} />
-      <PrivateRoute path="/Form/new" component={Form} />
+      <PrivateRoute path="/Form" component={Form} />
     </Switch>
   </BrowserRouter>
 );
