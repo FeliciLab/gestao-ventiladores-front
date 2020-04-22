a15fd612d324f4d3570b63588dc051af44aaf34d
Chicão Thiago <fthiagogv@gmail.com>
2020-04-21 21:45:10 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseUrl = 'https://api.centralventiladores.dev.org.br';
-const baseUrl = 'http://localhost:5000';
+const baseUrl = 'https://api.centralventiladores.dev.org.br';
+// const baseUrl = 'http://localhost:5000';
 
 const api = axios.create({
   headers: {
