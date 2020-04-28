4ebce16894508d694d158d827344dfd9ecb9eec6
Chicão Thiago <fthiagogv@gmail.com>
2020-04-28 17:27:32 -0300

    dev 0.4.10-dev

---
+++ precache-manifest.01c801133c45d545b1567387c05fa2af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e6e8fd326a9c5b3e8d4e2e86c63690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c28ea6493b1d9631cd43",
+    "url": "/static/css/main.7a2aec08.chunk.css"
+  },
+  {
+    "revision": "2ddedd9010837448ded4",
+    "url": "/static/js/2.25a84eb7.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/static/js/2.25a84eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28ea6493b1d9631cd43",
+    "url": "/static/js/main.9dbe617b.chunk.js"
+  },
+  {
+    "revision": "21489e95bc20c2c8f81e",
+    "url": "/static/js/runtime-main.371123ee.js"
+  }
+]);
