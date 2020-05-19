d43572e763a2da6bed1010f833a7df99266d749b
Chicão Thiago <fthiagogv@gmail.com>
2020-05-19 19:46:58 -0300

    link DeliveryPrint/index.jsx

--- src/router.js
+++ src/router.js
@@ -1,34 +1,34 @@
-import React from "react";
-import {Route, Switch} from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import IndexServiceOrder from "./pages/SerrviceOrder";
-import FormTriagem from "./pages/Screening/ScreeningForm";
-import IndexFormDiagnosis from "./pages/Diagnosis/DiagnosisForm";
-import OsPrint from "./MakeFile/OsPrint";
-import Screening from "./pages/Screening/index";
-import IndexDemand from "./pages/Demand";
-import Diagnosis from "./pages/Diagnosis";
-import IndexOrderPrint from "./MakeFile/OrderPrint";
-import Delivery from "./pages/Delivery";
+import IndexServiceOrder from './pages/SerrviceOrder';
+import FormTriagem from './pages/Screening/ScreeningForm';
+import IndexFormDiagnosis from './pages/Diagnosis/DiagnosisForm';
+import OsPrint from './MakeFile/OsPrint';
+import Screening from './pages/Screening/index';
+import IndexDemand from './pages/Demand';
+import Diagnosis from './pages/Diagnosis';
+import IndexOrderPrint from './MakeFile/OrderPrint';
+import Delivery from './pages/Delivery';
 import Calibration from './pages/Calibration';
-import IndexDeliveryPrint from './MakeFile/DeliveryPrint';
+import IndexDeliveryPrint from './MakeFile/DeliveryPrint/index';
 
 
 const Routes = () => (
   <Switch>
-    <Route path="/" exact component={IndexServiceOrder} />
-    <Route path="/ordens-servicos" exact component={IndexServiceOrder} />
-    <Route path="/triagens" exact component={Screening} />
-    <Route path="/nova-triagem" exact component={FormTriagem} />
-    <Route path="/editar-triagem" exact component={FormTriagem} />
-    <Route path="/novo-diagnostico" exact component={IndexFormDiagnosis} />
-    <Route path="/diagnosticos" exact component={Diagnosis} />
-    <Route path="/demandas" exact component={IndexDemand} />
-    <Route path="/ordem-compra" exact component={IndexOrderPrint} />
-    <Route path="/osprint" component={OsPrint} />
-    <Route path="/entregas" component={Delivery} />
-    <Route path="/entrega-impressao" component={IndexDeliveryPrint} />
-    <Route path="/calibragem" component={Calibration} />
+    <Route path="/" exact component={IndexServiceOrder}/>
+    <Route path="/ordens-servicos" exact component={IndexServiceOrder}/>
+    <Route path="/triagens" exact component={Screening}/>
+    <Route path="/nova-triagem" exact component={FormTriagem}/>
+    <Route path="/editar-triagem" exact component={FormTriagem}/>
+    <Route path="/novo-diagnostico" exact component={IndexFormDiagnosis}/>
+    <Route path="/diagnosticos" exact component={Diagnosis}/>
+    <Route path="/demandas" exact component={IndexDemand}/>
+    <Route path="/ordem-compra" exact component={IndexOrderPrint}/>
+    <Route path="/osprint" component={OsPrint}/>
+    <Route path="/entregas" component={Delivery}/>
+    <Route path="/entrega-impressao" component={IndexDeliveryPrint}/>
+    <Route path="/calibragem" component={Calibration}/>
   </Switch>
 );
 
