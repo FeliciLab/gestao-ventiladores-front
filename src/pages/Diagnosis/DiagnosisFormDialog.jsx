21d1c5183c8d64c961165c97c4291d9b447063e9
Chicão Thiago <fthiagogv@gmail.com>
2020-05-21 17:51:06 -0300

    Issue #52 pages/Diagnosis/

--- src/pages/Diagnosis/DiagnosisFormDialog.jsx
+++ src/pages/Diagnosis/DiagnosisFormDialog.jsx
@@ -1,105 +1,110 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import ThemeButton from '../_common/forms/ThemeButton';
 import CloseIcon from '@material-ui/icons/Close';
 import SaveIcon from '@material-ui/icons/Save';
 import orange from '@material-ui/core/colors/orange';
 import FormDiagnosis from './DiagnosisForm/FormDiagnosis';
+import ThemeButton from '../_common/forms/ThemeButton';
 import { updateServiceOrderRequest } from '../../modelServices/serviceOrderService';
 import FullDialog from '../_common/components/FullDialog';
 import { ServiceOrderDiagnosis } from '../../models/serviceOrder';
 
 
-export default function DiagnosisFormDialog (props) {
+const DiagnosisFormDialog = (props) => {
   const {
     handleClose,
     serviceOrder,
     openModal,
-    reloadData
+    reloadData,
   } = props;
 
   const [serviceOrderForm, setServiceOrderForm] = useState({});
 
+  const handleEffect = () => {
+    setServiceOrderForm(
+      { ...ServiceOrderDiagnosis({ dignostico: {} }), serviceOrder },
+    );
+  };
+
   useEffect(handleEffect, [serviceOrder]);
 
-  function handleEffect () {
+  const updateServiceForm = (value) => {
     setServiceOrderForm(
-      Object.assign(
-        {},
-        ServiceOrderDiagnosis({dignostico: {}}),
-        serviceOrder
-      )
+      { ...serviceOrder, diagnostico: serviceOrderForm.diagnostico, value },
     );
-  }
+  };
 
-  function updateServiceForm (value) {
-    setServiceOrderForm(
-      Object.assign(
-        {},
-        serviceOrder,
-        {diagnostico: serviceOrderForm.diagnostico},
-        value
-      )
-    );
-  }
-
-  async function saveForm () {
+  const saveForm = async () => {
     try {
       await updateServiceOrderRequest(
-        Object.assign(
-          {},
-          {diagnostico: serviceOrderForm.diagnostico},
-          {status: 'diagnostico'},
-          {updated_at: new Date()}
-        ),
-        serviceOrderForm._id['$oid']
+        {
+          diagnostico: serviceOrderForm.diagnostico,
+          status: 'diagnostico',
+          updated_at: new Date(),
+        },
+        serviceOrderForm._id.$oid,
       );
       reloadData();
+      return true;
     } catch (e) {
-      console.log('erro ao salvar ordem de serviço', e);
+      return false;
     }
-  }
+  };
 
-  const titleFormModal = serviceOrder.status === 'diagnostico' ?
-    `OS ${serviceOrder.numero_ordem_servico} - Editar Diagnóstico` :
-    `OS ${serviceOrder.numero_ordem_servico} - Cadastrar Diagnóstico`;
+  const titleFormModal = serviceOrder.status === 'diagnostico'
+    ? `OS ${serviceOrder.numero_ordem_servico} - Editar Diagnóstico`
+    : `OS ${serviceOrder.numero_ordem_servico} - Cadastrar Diagnóstico`;
 
-  return (<React.Fragment>
-    <FullDialog
-      open={openModal}
-      handleClose={handleClose}
-      title={titleFormModal}
-      actionChildren={<React.Fragment>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ThemeButton
-              onClick={handleClose}
-              startIcon={<CloseIcon/>}
-              variant={'outlined'}
-              borderColor={'white'}
-            >
-              Cancelar
-            </ThemeButton>
-          </Grid>
-          <Grid item>
-            <ThemeButton
-              startIcon={<SaveIcon/>}
-              onClick={saveForm}
-              name={'Salvar'}
-              color={orange[600]}
-              bgColor={'#FFF'}
-              hoverColor={orange[50]}
-            >
-              Salvar
-            </ThemeButton>
-          </Grid>
-        </Grid>
-      </React.Fragment>}
-    >
-      <FormDiagnosis
-        updateServiceForm={updateServiceForm}
-        serviceOrder={serviceOrderForm}
-      />
-    </FullDialog>
-  </React.Fragment>);
-}
+  return (
+    <>
+      <FullDialog
+        open={openModal}
+        handleClose={handleClose}
+        title={titleFormModal}
+        actionChildren={(
+          <>
+            <Grid container spacing={2}>
+              <Grid item>
+                <ThemeButton
+                  onClick={handleClose}
+                  startIcon={<CloseIcon />}
+                  variant="outlined"
+                  borderColor="white"
+                >
+                  Cancelar
+                </ThemeButton>
+              </Grid>
+              <Grid item>
+                <ThemeButton
+                  startIcon={<SaveIcon />}
+                  onClick={saveForm}
+                  name="Salvar"
+                  color={orange[600]}
+                  bgColor="#FFF"
+                  hoverColor={orange[50]}
+                >
+                  Salvar
+                </ThemeButton>
+              </Grid>
+            </Grid>
+          </>
+        )}
+      >
+        <FormDiagnosis
+          updateServiceForm={updateServiceForm}
+          serviceOrder={serviceOrderForm}
+        />
+      </FullDialog>
+    </>
+  );
+};
+
+DiagnosisFormDialog.propTypes = {
+  handleClose: PropTypes.func.isRequired,
+  serviceOrder: PropTypes.instanceOf(Object).isRequired,
+  openModal: PropTypes.bool.isRequired,
+  reloadData: PropTypes.func.isRequired,
+};
+
+export default DiagnosisFormDialog;
