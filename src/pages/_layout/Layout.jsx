e13fe40802d547a2516a86418c7b09ed1c5f17e5
Jo Correa <joseline.correa@gmail.com>
2020-05-29 18:30:46 -0300

    [Flavia/Jo] Issue #69: Refatoração do componente Header

--- src/pages/_layout/Layout.jsx
+++ src/pages/_layout/Layout.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import grey from '@material-ui/core/colors/grey';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import Header from './Header';
+import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Menu from '../../components/Menu/Menu';
 
 const useStyle = makeStyles(() => ({
   layout: {
@@ -22,6 +23,7 @@ const Layout = (props) => {
   return (
     <div className={classes.layout}>
       <Header />
+      <Menu />
       {children}
       <Footer />
     </div>
