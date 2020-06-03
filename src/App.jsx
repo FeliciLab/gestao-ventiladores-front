4795c3493ca0308089f2d33f4f88d958de88d47d
Jo Correa <joseline.correa@gmail.com>
2020-06-03 18:12:30 -0300

    [Ju/Jo] #69 - Altera formato de App.js para .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import './global.css';
 import './pages/_common/print/print.css';
 import './pages/_common/print/portraitCss.css';
 import './pages/_common/print/landscapeCss.css';
-import { BrowserRouter } from 'react-router-dom';
 import Routes from './router';
-
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Routes />
     </>
   );
 }
