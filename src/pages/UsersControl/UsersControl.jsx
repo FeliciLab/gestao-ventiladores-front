2d5f971a7711a5b6ee7087c089de483739d1052d
Jo Correa <joseline.correa@gmail.com>
2020-06-05 17:37:57 -0300

    [#61][Flavia|Jo] Implementação de estilos para o componente UsersControl

--- src/pages/UsersControl/UsersControl.jsx
+++ src/pages/UsersControl/UsersControl.jsx
@@ -9,14 +9,67 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Checkbox from '@material-ui/core/Checkbox';
 import { makeStyles } from '@material-ui/core/styles';
+import SendIcon from '@material-ui/icons/Send';
+import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
+import DoneIcon from '@material-ui/icons/Done';
+import ThemeButton from '../_common/forms/ThemeButton';
 import Layout from '../_layout/Layout';
 
 const useStyle = makeStyles(() => ({
-  containerHeader: {
+  page: {
+    padding: '55px 0px',
+  },
+  pageHeader: {
     display: 'flex',
     justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  paperSection: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'normal',
+    textTransform: 'uppercase',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  paper: {
+    marginTop: 55,
+    padding: 10,
+  },
+  paperSectionTitle: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    color: '#BDBDBD',
+    marginLeft: 20,
+  },
+  table: {
+    marginTop: 20,
+    marginBottom: 20,
+    '& th': {
+      padding: 0,
+      fontWeight: 'bold',
+    },
+    '& td': {
+      padding: 0,
+    },
+    '& span': {
+      color: 'rgba(0, 0, 0, 0.87)',
+      '&:hover': {
+        backgroundColor: 'rgba(0, 0, 0, 0.04)',
+      },
+    },
+  },
+  smallTitle: {
+    fontWeight: 'normal',
+    fontSize: 12,
+    color: 'rgba(0, 0, 0, 0.54)',
   },
 }));
+const labels = ['Ordem', 'Nome', 'Unidade', 'ID Saúde', 'Data de Solicitação'];
 
 const acessos = [
   {
@@ -42,58 +95,95 @@ const acessos = [
   },
 ];
 
+const createTableHeader = (tableLabels) => (
+  <TableHead>
+    <TableRow>
+      <TableCell>
+        <Checkbox
+        // indeterminate={numSelected > 0 && numSelected < rowCount}
+        // checked={rowCount > 0 && numSelected === rowCount}
+        // onChange={onSelectAllClick}
+        // inputProps={{ 'aria-label': 'select all desserts' }}
+        // eslint-disable-next-line react/jsx-closing-bracket-location
+        />
+      </TableCell>
+      {tableLabels.map((label) => (
+        <TableCell key={label.toLocaleUpperCase()}>{label}</TableCell>
+      ))}
+    </TableRow>
+  </TableHead>
+);
+
+const createTableBody = (data) => (
+  <TableBody>
+    {data.map((item) => (
+      <TableRow key={item.idSaude}>
+        <TableCell>
+          <Checkbox
+            // eslint-disable-next-line no-console
+            onChange={() => console.log(item.idSaude)}
+            inputProps={{ 'aria-label': 'select item' }}
+            // eslint-disable-next-line react/jsx-closing-bracket-location
+          />
+        </TableCell>
+        <TableCell>{item.ordem}</TableCell>
+        <TableCell>{item.nome}</TableCell>
+        <TableCell>{item.unidade}</TableCell>
+        <TableCell>{item.idSaude}</TableCell>
+        <TableCell>{item.dataSolicitacao}</TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+);
+
 const UsersControl = () => {
   const classes = useStyle();
 
   return (
     <Layout>
-      <Container>
-        <div className={classes.containerHeader}>
-          <h1>Fila de Solicitação de acesso</h1>
-          <button type="submit">Enviar convite</button>
+      <Container className={classes.page}>
+        <div className={classes.pageHeader}>
+          <h1 className={classes.title}>Fila de Solicitação de acesso</h1>
+          <ThemeButton
+            startIcon={<SendIcon />}
+            // eslint-disable-next-line no-console
+            onClick={() => console.log('Clicou em enviar')}>
+            Enviar convite
+          </ThemeButton>
         </div>
 
-        <Paper>
-          <div className={classes.containerHeader}>
-            <h2>Selecione os itens para conceder acesso</h2>
-            <button type="submit">Conceder acesso aos usuários</button>
+        <Paper className={classes.paper}>
+          <div className={classes.paperSection}>
+            <h2 className={classes.paperSectionTitle}>
+              Selecione os itens para conceder acesso
+            </h2>
+            <Button disabled variant="outlined">
+              Conceder acesso aos usuários
+            </Button>
           </div>
 
-          <TableContainer>
+          <TableContainer className={classes.table}>
             <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>
-                    <Checkbox
-                      indeterminate={numSelected > 0 && numSelected < rowCount}
-                      checked={rowCount > 0 && numSelected === rowCount}
-                      onChange={onSelectAllClick}
-                      inputProps={{ 'aria-label': 'select all desserts' }}
-                    />
-                  </TableCell>
-                  <TableCell>t</TableCell>
-                  <TableCell>a</TableCell>
-                  <TableCell>t</TableCell>
-                  <TableCell>u</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>t</TableCell>
-                  <TableCell>a</TableCell>
-                  <TableCell>t</TableCell>
-                  <TableCell>u</TableCell>
-                </TableRow>
-              </TableBody>
+              {createTableHeader(labels)}
+              {createTableBody(acessos)}
             </Table>
           </TableContainer>
 
-          <h2>Itens selecionados</h2>
-          <div className={classes.containerHeader}>
+          <h2 className={classes.smallTitle}>Itens selecionados</h2>
+          <div className={classes.paperSection}>
             <div>
-              <div>x flag</div>
+              <div>
+                <Chip
+                  icon={<DoneIcon />}
+                  label="001"
+                  // onDelete={data.label === 'React' ? undefined : handleDelete(data)}
+                  className={classes.chip}
+                />
+              </div>
             </div>
-            <button type="submit">Conceder acesso aos usuários</button>
+            <Button disabled variant="outlined">
+              Conceder acesso aos usuários
+            </Button>
           </div>
         </Paper>
       </Container>
