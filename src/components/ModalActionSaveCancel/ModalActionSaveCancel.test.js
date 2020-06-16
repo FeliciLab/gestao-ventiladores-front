2f4e74f6a33e806dfd19f39242c783368b8866b5
Chicão Thiago <fthiagogv@gmail.com>
2020-06-16 16:26:22 -0300

    Criado um componente bastante utilizado quando se usa o component fulldialog, afim de evitar reescrever a mesma estrutura em todos os formulários.

---
+++ src/components/ModalActionSaveCancel/ModalActionSaveCancel.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import ModalActionSaveCancel from './ModalActionSaveCancel';
+
+const error = new Error(
+  'handleSave or handleCancel props are undefined or not a function',
+);
+
+describe('<ModalActionSaveCancel />', () => {
+  it('should render component with handleSave and handleCancel', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <ModalActionSaveCancel
+          handleSave={() => true}
+          handleCancel={() => false}
+        />
+      </BrowserRouter>,
+    );
+    expect(container).toBeDefined();
+  });
+  it('should not render component without handleSave', () => {
+    expect(() =>
+      render(<ModalActionSaveCancel handleCancel={jest.fn()} />),
+    ).toThrow(error);
+  });
+  it('should not render component without handleCancel', () => {
+    expect(() =>
+      render(<ModalActionSaveCancel handleSave={jest.fn()} />),
+    ).toThrow(error);
+  });
+});
