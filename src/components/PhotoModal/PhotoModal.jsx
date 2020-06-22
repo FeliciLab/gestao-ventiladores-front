3728a8f61a7e54727c19a08f8535af1de62cd9f0
Chicão Thiago <fthiagogv@gmail.com>
2020-06-22 15:18:17 -0300

    [@chicaothiago/@lauralucca/@Edely/@juliakastrup] #89 Modal da lista de ordem de serviço

--- src/components/PhotoModal/PhotoModal.jsx
+++ src/components/PhotoModal/PhotoModal.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from '@material-ui/core/Grid';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 
 const Photo = ({ src, alt }) => {
   const styleImg = {
     background: `url(${src})`,
     backgroundSize: 'cover',
+    backgroundPosition: 'center',
     width: '100%',
     height: '100%',
     display: 'flex',
@@ -20,16 +26,46 @@ const Photo = ({ src, alt }) => {
   };
 
   return (
-    <>
-      <div style={styleImg}>
-        <div style={styleLabel}>
-          <p>{alt}</p>
-        </div>
+    <div style={styleImg}>
+      <div style={styleLabel}>
+        <p>{alt}</p>
       </div>
-    </>
+    </div>
   );
 };
+
 Photo.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+const DialogPhoto = ({ open, close, src, alt }) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={close}
+      aria-labelledby="imagem-do-equipamento"
+      aria-describedby="imagem-do-equipamento"
+      fullWidth
+      maxWidth="lg">
+      <DialogTitle id="alert-dialog-title">
+        <Grid container justify="space-between" alignItems="center">
+          <Grid item>{alt}</Grid>
+          <Grid item>
+            <IconButton onClick={close}>
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </DialogTitle>
+      <img src={src} alt={alt} width="100%" />
+    </Dialog>
+  );
+};
+
+DialogPhoto.propTypes = {
+  open: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 };
@@ -47,6 +83,12 @@ const PhotoModal = ({ src, alt }) => {
         tabIndex={0}>
         <Photo src={src} alt={alt} />
       </div>
+      <DialogPhoto
+        open={open}
+        close={() => setOpen(false)}
+        src={src}
+        alt={alt}
+      />
     </>
   );
 };
