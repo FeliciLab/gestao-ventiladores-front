c0dd8e57459aac2ef7472a482edb54b347359cc8
Chicão Thiago <fthiagogv@gmail.com>
2020-06-22 19:17:06 -0300

    2.0.0-homolog

---
+++ precache-manifest.236aab8470da6ed0f29e4a34b9d81b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2108e403328c0c47bc2f5fa1be3e3601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76adfa0ad2bccd5f3821",
+    "url": "/static/css/main.7a2aec08.chunk.css"
+  },
+  {
+    "revision": "9765dba981551750c6f7",
+    "url": "/static/js/2.d74f44f9.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/static/js/2.d74f44f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76adfa0ad2bccd5f3821",
+    "url": "/static/js/main.db4d4357.chunk.js"
+  },
+  {
+    "revision": "e3c7aa3da3f4cdd2f324",
+    "url": "/static/js/runtime-main.d00d5456.js"
+  },
+  {
+    "revision": "90ac1e5008e6ccd595d4df71529011b6",
+    "url": "/static/media/doctors-sidebar.90ac1e50.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d744e56abaf9018bbd002942e0fde09.js"
+  "/precache-manifest.236aab8470da6ed0f29e4a34b9d81b9c.js"
 );
 
 self.addEventListener('message', (event) => {
