093a219e58be05608638c63c21be55d3bf48bf20
Laura L de Lucca <lauralucca@gmail.com>
2020-06-26 02:22:06 -0300

    [Laura] Merge branch 'develop' of https://github.com/EscolaDeSaudePublica/gestao-ventiladores-front into feature/issue-75-login

--- src/pages/_common/components/FullDialog.jsx
+++ src/pages/_common/components/FullDialog.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CloseIcon from '@material-ui/icons/Close';
-import {
-  AppBar,
-  Dialog,
-  Grid,
-  IconButton,
-  Toolbar,
-  Typography,
-} from '@material-ui/core';
+import { AppBar, Dialog, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import orange from '@material-ui/core/colors/orange';
+
 
 const useStyle = makeStyles((theme) => ({
   appBar: {
@@ -18,21 +12,23 @@ const useStyle = makeStyles((theme) => ({
     backgroundColor: orange[600],
   },
   fullDialogBody: {
-    paddingTop: theme.spacing(10),
-  },
+    paddingTop: theme.spacing(10)
+  }
 }));
 
 const FullDialog = (props) => {
   const classes = useStyle();
-  const { open, title, handleClose, children, actionChildren } = props;
+  const {
+    open,
+    title,
+    handleClose,
+    children,
+    actionChildren,
+  } = props;
 
   return (
     <>
-      <Dialog
-        fullScreen
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title">
+      <Dialog fullScreen open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
         <AppBar className={classes.appBar}>
           <Toolbar>
             <Grid container alignItems="center" justify="space-between">
@@ -43,20 +39,27 @@ const FullDialog = (props) => {
                       edge="start"
                       color="inherit"
                       onClick={handleClose}
-                      aria-label="close">
+                      aria-label="close"
+                    >
                       <CloseIcon />
                     </IconButton>
                   </Grid>
                   <Grid item>
-                    <Typography variant="h6">{title}</Typography>
+                    <Typography variant="h6">
+                      {title}
+                    </Typography>
                   </Grid>
                 </Grid>
               </Grid>
-              <Grid item>{actionChildren}</Grid>
+              <Grid item>
+                {actionChildren}
+              </Grid>
             </Grid>
           </Toolbar>
         </AppBar>
-        <div className={classes.fullDialogBody}>{children}</div>
+        <div className={classes.fullDialogBody}>
+          {children}
+        </div>
       </Dialog>
     </>
   );
@@ -64,7 +67,7 @@ const FullDialog = (props) => {
 
 FullDialog.defaultProps = {
   open: false,
-  actionChildren: <></>,
+  actionChildren: (<></>),
 };
 
 FullDialog.propTypes = {
