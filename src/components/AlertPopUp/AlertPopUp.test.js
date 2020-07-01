c2a696a9f2ce2a2cd8487321c077a63399fb645c
Chicão Thiago <fthiagogv@gmail.com>
2020-07-01 12:40:06 -0300

    [@chicaothiago/@josscorrea/@lauralucca] Componente de pop-up alerta global

---
+++ src/components/AlertPopUp/AlertPopUp.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+import AlertPopUp from './AlertPopUp';
+import AlertContext from '../../context/AlertContext';
+
+
+describe('<AlertPopUp/>', () => {
+  it('should render with valid message', () => {
+    render(
+      <AlertContext.Provider
+        value={{
+          alert: { message: 'Eita, joia xablau!', alertType: 'success' },
+          setAlertMessage: jest.fn(),
+        }}
+      >
+        <AlertPopUp />
+      </AlertContext.Provider>,
+    );
+
+    expect(screen.getByTestId('alert-message')).toBeInTheDocument();
+  });
+
+  it('should show valid message in context', () => {
+    render(
+      <AlertContext.Provider
+        value={{
+          alert: { message: 'Eita, joia xablau!', alertType: 'success' },
+          setAlertMessage: jest.fn(),
+        }}
+      >
+        <AlertPopUp />
+      </AlertContext.Provider>,
+    );
+
+    expect(screen.getByText('Eita, joia xablau!')).toBeInTheDocument();
+  });
+
+  it('should not render without message in context', () => {
+    render(
+      <AlertContext.Provider
+        value={{
+          alert: { message: '', alertType: 'success' },
+          setAlertMessage: jest.fn(),
+        }}
+      >
+        <AlertPopUp />
+      </AlertContext.Provider>,
+    );
+
+    expect(screen.queryByText('Eita, joia xablau!')).toBe(null);
+    expect(screen.queryByTestId('alert-message')).toBe(null);
+  });
+});
