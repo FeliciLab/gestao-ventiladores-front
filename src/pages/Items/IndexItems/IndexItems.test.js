65843c0233784b3f31edaad26a955d52fc090c83
Chicão Thiago <fthiagogv@gmail.com>
2020-07-15 16:23:55 -0300

    [@chicaothiago/@lauralucca] #60 - Reabilitando os teste de itempage

--- src/pages/Items/IndexItems/IndexItems.test.js
+++ src/pages/Items/IndexItems/IndexItems.test.js
@@ -1,50 +1,68 @@
 import React from 'react';
-// import { render } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
-// import IndexItems from './IndexItems';
+import IndexItems from './IndexItems';
+import { AlertProvider } from '../../../contexts/AlertContext';
+import { LoadingProvider } from '../../../contexts/LoadingContext';
 
 describe('<IndexItems/>', () => {
-  it('ok', () => {})
-  // describe('<LoadingBar />', () => {
-  //   it('should render LoadingBar with no props be sent', () => {
-  //     const { getByTestId } = render(
-  //       <BrowserRouter>
-  //         <IndexItems />
-  //       </BrowserRouter>,
-  //     );
-  //     expect(getByTestId('loadingBar')).toBeInTheDocument();
-  //   });
-  //
-  //   it('should render LoadingBar with loading true', () => {
-  //     const { getByTestId } = render(
-  //       <BrowserRouter>
-  //         <IndexItems loading={true} />
-  //       </BrowserRouter>,
-  //     );
-  //     expect(getByTestId('loadingBar')).toBeInTheDocument();
-  //   });
-  //
-  //   it('should not render LoadingBar with loading false', async () => {
-  //     const { queryByTestId } = render(
-  //       <BrowserRouter>
-  //         <IndexItems loading={false} />
-  //       </BrowserRouter>,
-  //     );
-  //
-  //     expect(queryByTestId('loadingBar')).toBeNull();
-  //   });
-  // });
-  //
-  // describe('<ItemPage>', () => {
-  //   it('should render ItemPage with loading false', async () => {
-  //     const { queryByTestId } = render(
-  //       <BrowserRouter>
-  //         <IndexItems loading={false} />
-  //       </BrowserRouter>,
-  //     );
-  //
-  //     expect(queryByTestId('itemPage')).not.toBeNull();
-  //   });
-  // });
+  describe('<LoadingBar />', () => {
+    it('should render LoadingBar with no props be sent', () => {
+      const { getByTestId } = render(
+        <BrowserRouter>
+          <AlertProvider>
+            <LoadingProvider>
+              <IndexItems />
+            </LoadingProvider>
+          </AlertProvider>
+        </BrowserRouter>,
+      );
+
+      expect(getByTestId('loadingBar')).toBeInTheDocument();
+    });
+
+    it('should render LoadingBar with loading true', () => {
+      const { getByTestId } = render(
+        <BrowserRouter>
+          <AlertProvider>
+            <LoadingProvider>
+              <IndexItems loading={true} />
+            </LoadingProvider>
+          </AlertProvider>
+        </BrowserRouter>,
+      );
+      expect(getByTestId('loadingBar')).toBeInTheDocument();
+    });
+
+    it('should not render LoadingBar with loading false', async () => {
+      const { queryByTestId } = render(
+        <BrowserRouter>
+          <AlertProvider>
+            <LoadingProvider>
+              <IndexItems loading={false} />
+            </LoadingProvider>
+          </AlertProvider>
+        </BrowserRouter>,
+      );
+
+      expect(queryByTestId('loadingBar')).toBeNull();
+    });
+  });
+
+  describe('<ItemPage>', () => {
+    it('should render ItemPage with loading false', async () => {
+      const { queryByTestId } = render(
+        <BrowserRouter>
+          <AlertProvider>
+            <LoadingProvider>
+              <IndexItems loading={false} />
+            </LoadingProvider>
+          </AlertProvider>
+        </BrowserRouter>,
+      );
+
+      expect(queryByTestId('itemPage')).not.toBeNull();
+    });
+  });
 });
